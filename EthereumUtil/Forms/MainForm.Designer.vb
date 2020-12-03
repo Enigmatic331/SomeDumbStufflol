14dad2b00eb60303fede7904944de66247ec6eaa
Enigmatic331 <unreal_legend7@hotmail.com>
2020-12-03 16:05:03 +0800

    .

---
+++ EthereumUtil/Forms/MainForm.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnExec = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnExec
+        '
+        Me.btnExec.Location = New System.Drawing.Point(6, 21)
+        Me.btnExec.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnExec.Name = "btnExec"
+        Me.btnExec.Size = New System.Drawing.Size(474, 32)
+        Me.btnExec.TabIndex = 0
+        Me.btnExec.Text = "Exec"
+        Me.btnExec.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 105)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExec)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "Test"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExec As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ EthereumUtil/Forms/MainForm.vb
@@ -0,0 +1,38 @@
+﻿
+Imports Nethereum.Web3
+Imports Nethereum.Util
+Imports Nethereum.RPC
+
+Public Class MainForm
+
+    Private Sub btnExec_Click(sender As Object, e As EventArgs) Handles btnExec.Click
+        Call GetBalance()
+    End Sub
+
+    Private Async Sub GetBalance()
+        ' from account - account we are transferring from
+        Dim privateKey As New Nethereum.Signer.EthECKey("0000000000000000000000000000000000000000000000000000000000000001")
+        Dim account = New Nethereum.Web3.Accounts.Account(privateKey)
+        Dim stopMe As Boolean = False
+
+        While stopMe <> True
+            Label1.Text = "Checking to see if it is happening..."
+            Label1.Refresh()
+
+            Dim iweb3 = New Web3(account, "https://cloudflare-eth.com")
+
+            Dim contractAddress = "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+            Dim balance = Await iweb3.Eth.GetBalance.SendRequestAsync("0x00000000219ab540356cBB839Cbe05303d7705Fa")
+
+            If balance.Value >= Web3.Convert.ToWei(1000000, UnitConversion.EthUnit.Ether) Then
+                MsgBox("IT HAPPENED!")
+                stopMe = True
+            Else
+                Label1.Text = "Checking to see if it is happening.... Nope. Not yet. Sleep for 5 secs first."
+                Label1.Refresh()
+                System.Threading.Thread.Sleep(5000)
+            End If
+        End While
+    End Sub
+
+End Class
